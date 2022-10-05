a6dc423328a4a27883c048152a422ff57821cd7f
Musa Fındık <mfindik@outlook.com>
2022-10-06 01:07:41 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿namespace KampaGiris
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Type safety - Tip güvenliği
+            //Do not repeat yourself - Kendini tekrarlama
+
+            string kategoriEtiketi = "Kategori";
+            int ogrenciSayisi = 32000;
+            double faizOrani = 1.45;
+            bool sistemeGirisYapmisMi = false;
+            double dolarDun = 7.35;
+            double dolarBugun = 7.45;
+
+            if (dolarDun > dolarBugun)
+            {
+                Console.WriteLine("Azalış butonu");
+            }
+            else if (dolarDun < dolarBugun)
+            {
+                Console.WriteLine("Yükseliş butonu");
+            }
+            else
+            {
+                Console.WriteLine("Diğişiklik yok butonu");
+            }
+
+
+
+
+            if (sistemeGirisYapmisMi == true)
+            {
+                Console.WriteLine("Kullanıcı ayarları butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giris yap butonu");
+            }
+
+            Console.WriteLine(kategoriEtiketi);
+
+        }
+    }
+}
